ca84d459f0ba48fa6d374b25e10f0d3a7dcebce3
jangwon <vcinser@naver.com>
2023-11-12 14:26:27 +0900

    project init

---
+++ android/app/src/main/kotlin/com/example/test_11_12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_11_12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:test_11_12/home.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home(),));
+}
